2d30c53c8d8cee8d0b2dd972e56c0ebd4674e3ef
gestagene <gestagenebrian@gmail.com>
2026-05-23 01:58:31 +0800

    feat(ui): add analytics screen

--- src/app/(tabs)/Analytics.tsx
+++ src/app/(tabs)/Analytics.tsx
@@ -1,26 +1,94 @@
 import Header from "@/components/Header";
 import { colors } from "@/constants/colorscheme";
-import { StyleSheet, Text, View } from "react-native";
+import { router } from "expo-router";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Analytics() {
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView edges={["top"]} style={styles.container}>
       <Header content={"Analytics"} />
-      <View style={styles.main}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          flexGrow: 1,
+          paddingHorizontal: 16,
+          paddingVertical: 2,
+          paddingBottom: 32,
+          backgroundColor: colors.primary,
+          minHeight: "100%",
+        }}
+        style={styles.main}
+      >
         <View style={styles.header}>
-          <Text style={styles.headerText}>PLACEHOLDER</Text>
-          <View
-            style={{
-              borderBottomWidth: 0.5,
-              borderColor: "#ffffff",
-              opacity: 0.5,
-              width: "50%",
-              height: 1,
-            }}
-          ></View>
+          <Text style={styles.headerText}>Performance Snapshot</Text>
         </View>
-      </View>
+        <View style={styles.card}></View>
+        <View
+          style={[
+            styles.section,
+            { justifyContent: "center", alignItems: "center" },
+          ]}
+        >
+          <Text adjustsFontSizeToFit numberOfLines={1} style={styles.text}>
+            Tip: Lorem Ipsum Dolem Sahur
+          </Text>
+        </View>
+        <View style={styles.grid}>
+          <View style={styles.gridCard}>
+            <Text style={[styles.text, { fontWeight: "700" }]}>
+              Avg. Calories
+            </Text>
+          </View>
+          <View style={styles.gridCard}>
+            <Text style={[styles.text, { fontWeight: "700" }]}>
+              Avg. Protein
+            </Text>
+          </View>
+          <View style={styles.gridCard}>
+            <Text style={[styles.text, { fontWeight: "700" }]}>
+              Workout Frequency
+            </Text>
+          </View>
+          <View style={styles.gridCard}>
+            <Text style={[styles.text, { fontWeight: "700" }]}>Steps</Text>
+          </View>
+        </View>
+        <View
+          style={[
+            styles.section,
+            { gap: 6, justifyContent: "center", alignItems: "center" },
+          ]}
+        >
+          <Text style={[styles.headerText, { textAlign: "center" }]}>
+            Personal Achievements
+          </Text>
+          <TouchableOpacity
+            disabled={true}
+            onPress={() => router.push("/analytics/Achievements")}
+            style={[
+              styles.card,
+              {
+                height: 45,
+                width: "85%",
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: "#444",
+                borderRadius: 20,
+                opacity: 0.5,
+              },
+            ]}
+          >
+            <Text style={styles.headerText}>Coming soon</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -33,21 +101,47 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     backgroundColor: colors.primary,
-    paddingHorizontal: 16,
-    paddingVertical: 2,
-    minHeight: "100%",
   },
   header: {
     flexDirection: "column",
-    marginTop: 12,
-    marginBottom: 16,
+    marginVertical: 12,
     alignItems: "center",
     gap: 6,
   },
+  card: {
+    backgroundColor: colors.tertiary,
+    width: "100%",
+    height: 200,
+    borderRadius: 6,
+    marginVertical: 4,
+  },
   headerText: {
     fontWeight: "900",
-    fontSize: 16,
+    fontSize: 18,
     color: "#ffffff",
     letterSpacing: 0.5,
+  },
+  section: {
+    marginVertical: 8,
+  },
+  text: {
+    color: colors.textSecondary,
+    fontSize: 12,
+    letterSpacing: 0.5,
+    fontWeight: 300,
+  },
+  grid: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginVertical: 8,
+  },
+  gridCard: {
+    width: "48%",
+    height: 140,
+    backgroundColor: colors.tertiary,
+    borderRadius: 6,
+    padding: 12,
+    marginBottom: 12,
   },
 });
