dbbc13c5142f9f79e3812ac5a4eb4cbee1e8d302
gestagene <gestagenebrian@gmail.com>
2026-05-23 02:00:04 +0800

    feat: add optional color props

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,8 +4,9 @@ import * as Progress from "react-native-progress";
 type ProgressProps = {
   target: number;
   value: number;
+  color?: string;
 };
-export default function ProgressBar({ target, value }: ProgressProps) {
+export default function ProgressBar({ target, value, color }: ProgressProps) {
   const progress = value / target;
   return (
     <Progress.Bar
@@ -13,7 +14,7 @@ export default function ProgressBar({ target, value }: ProgressProps) {
       width={null}
       height={8}
       borderRadius={100}
-      color={colors.accent}
+      color={color ?? colors.accent}
       unfilledColor={colors.primary}
       borderWidth={0}
     />
